9e065d7ace62a0509fd18f3ce3fd36f1e140a3c9
djhill325 <64169886+djhill325@users.noreply.github.com>
2020-11-28 20:08:39 -0800

    upload Reflectance.c
    Solution to TI RSLK MAX Module 6.
    See Lab06_GPIOmain.c for examples of how to call the functions.

---
+++ Reflectance.c
@@ -0,0 +1,181 @@
+// Reflectance.c
+// Provide functions to take measurements using the kit's built-in
+// QTRX reflectance sensor array.  Pololu part number 3672. This works by outputting to the
+// sensor, waiting, then reading the digital value of each of the
+// eight phototransistors.  The more reflective the target surface is,
+// the faster the voltage decays.
+// Daniel and Jonathan Valvano
+// July 11, 2019
+
+/* This example accompanies the book
+   "Embedded Systems: Introduction to Robotics,
+   Jonathan W. Valvano, ISBN: 9781074544300, copyright (c) 2019
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+
+Simplified BSD License (FreeBSD License)
+Copyright (c) 2019, Jonathan Valvano, All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are
+those of the authors and should not be interpreted as representing official
+policies, either expressed or implied, of the FreeBSD Project.
+*/
+
+// reflectance even LED illuminate connected to P5.3
+// reflectance odd LED illuminate connected to P9.2
+// reflectance sensor 1 connected to P7.0 (robot's right, robot off road to left)
+// reflectance sensor 2 connected to P7.1
+// reflectance sensor 3 connected to P7.2
+// reflectance sensor 4 connected to P7.3 center
+// reflectance sensor 5 connected to P7.4 center
+// reflectance sensor 6 connected to P7.5
+// reflectance sensor 7 connected to P7.6
+// reflectance sensor 8 connected to P7.7 (robot's left, robot off road to right)
+
+#include <stdint.h>
+#include "msp432.h"
+#include "..\inc\Clock.h"
+
+// ------------Reflectance_Init------------
+// Initialize the GPIO pins associated with the QTR-8RC
+// reflectance sensor.  Infrared illumination LEDs are
+// initially off.
+// Input: none
+// Output: none
+void Reflectance_Init(void){
+    // write this as part of Lab 6
+    P5->SEL0 &= ~0x04;
+    P5->SEL1 &= ~0x04;   // 1) configure P5.3 as GPIO
+    P5->DIR |= 0x04;     // 2) make P5.3 out
+    P5->OUT &= ~0x04;     // set low
+
+    P9->SEL0 &= ~0x02;
+    P9->SEL1 &= ~0x02;   // 1) configure P9.2 as GPIO
+    P9->DIR |= 0x02;     // 2) make P9.2 out
+    P9->OUT &= ~0x02;     // set low
+
+    // make P7.0-7.7 an input
+    P7->SEL0 &= ~0xff;
+    P7->SEL1 &= ~0xff;    // 1) configure P7.0-7.7 as GPIO
+    P7->DIR &= ~0xff;     // 2) make P7.0-7.7 in
+
+    //make P4 an output
+    P4->SEL0 &= ~0xff;
+    P4->SEL1 &= ~0xff;   // 1) configure P4.0 as GPIO
+    P4->DIR |= 0xff;     // 2) make P4.0 out
+}
+
+// ------------Reflectance_Read------------
+// Read the eight sensors
+// Turn on the 8 IR LEDs
+// Pulse the 8 sensors high for 10 us
+// Make the sensor pins input
+// wait t us
+// Read sensors
+// Turn off the 8 IR LEDs
+// Input: time to wait in usec
+// Output: sensor readings
+// Assumes: Reflectance_Init() has been called
+uint8_t Reflectance_Read(uint32_t time){
+    // write this as part of Lab 6
+
+  P5->OUT |= 0x04;     // set 5.3 high
+  P9->OUT |= 0x02;     // set 9.2 high
+
+  P7->DIR |= 0xFF;  // make 7.0-7.7 output
+  P7->OUT |= 0xFF;     // set 7.0-7.7 high (charge cap)
+  Clock_Delay1us(10); // wait 10 us
+  P7->DIR &= ~0xff;     // 2) make P7.0-7.7 in
+  Clock_Delay1us(time);
+  P4->OUT = P7->IN&0xFF; // convert P7.0-7.7 input to digital, output to P4.0
+  P5->OUT &= ~0x04;     // set 5.3 low
+  P9->OUT &= ~0x02;     // set 9.2 low
+
+  return P4->OUT;
+}
+
+// ------------Reflectance_Center------------
+// Read the two center sensors
+// Turn on the 8 IR LEDs
+// Pulse the 8 sensors high for 10 us
+// Make the sensor pins input
+// wait t us
+// Read sensors
+// Turn off the 8 IR LEDs
+// Input: time to wait in usec
+// Output: 0 (off road), 1 off to left, 2 off to right, 3 on road
+// (Left,Right) Sensors
+// 1,1          both sensors   on line
+// 0,1          just right     off to left
+// 1,0          left left      off to right
+// 0,0          neither        lost
+// Assumes: Reflectance_Init() has been called
+uint8_t Reflectance_Center(uint32_t time){
+    // write this as part of Lab 6
+  return 0; // replace this line
+}
+
+
+// Perform sensor integration
+// Input: data is 8-bit result from line sensor
+// Output: position in 0.1mm relative to center of line
+int32_t Reflectance_Position(uint8_t data){
+    // write this as part of Lab 6
+    int32_t result = 0;
+    int32_t bSum = 0;
+    int32_t W[8] = {334, 238, 142, 48, -48, -142, -238, -334};
+    for (int i = 0; i < 8; i++) {
+        result = result + W[i] * (data & 1);
+        bSum = bSum + (data & 1);
+        data = data >> 1;
+    }
+ return result/bSum;
+}
+
+
+// ------------Reflectance_Start------------
+// Begin the process of reading the eight sensors
+// Turn on the 8 IR LEDs
+// Pulse the 8 sensors high for 10 us
+// Make the sensor pins input
+// Input: none
+// Output: none
+// Assumes: Reflectance_Init() has been called
+void Reflectance_Start(void){
+    // write this as part of Lab 10
+}
+
+
+// ------------Reflectance_End------------
+// Finish reading the eight sensors
+// Read sensors
+// Turn off the 8 IR LEDs
+// Input: none
+// Output: sensor readings
+// Assumes: Reflectance_Init() has been called
+// Assumes: Reflectance_Start() was called 1 ms ago
+uint8_t Reflectance_End(void){
+    // write this as part of Lab 10
+ return 0; // replace this line
+}
+
